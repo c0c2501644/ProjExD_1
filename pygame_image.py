b90ef52e8323769b59bff47190e06b40a5e4583e
c0c2501644 <c0c2501644@edu.teu.ac.jp>
2026-04-14 15:46:55 +0900

    練習3,4

--- pygame_image.py
+++ pygame_image.py
@@ -9,13 +9,15 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img = pg.image.load("fig/pg_bg.jpg") #練習1
+    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.flip(kk_img, True, False) #練習3
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])
+        screen.blit(bg_img, [0, 0]) #練習2
+        screen.blit(kk_img, [300, 200])#練習4
         pg.display.update()
         tmr += 1        
         clock.tick(10)
