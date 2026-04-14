4d2626daef9183a6d5cf02aa0bf97ddd93a679c1
c0c2501644 <c0c2501644@edu.teu.ac.jp>
2026-04-14 17:07:34 +0900

    課題１

--- pygame_image.py
+++ pygame_image.py
@@ -1,47 +1,80 @@
 import os
+
 import sys
+
 import pygame as pg
+
+
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
+
 def main():
+
     pg.display.set_caption("はばたけ！こうかとん")
+
     screen = pg.display.set_mode((800, 600))
+
     clock  = pg.time.Clock()
+
     bg_img = pg.image.load("fig/pg_bg.jpg") #練習1
+
     bg_img2 = pg.transform.flip(bg_img, True, False) #練習8
+
     kk_img = pg.image.load("fig/3.png")#練習3
+
     kk_img = pg.transform.flip(kk_img, True, False)
 
     kk_rct = kk_img.get_rect() #練習10.1
+
     kk_rct.center = 300,200 #練習10.2
+
     tmr = 0
+
     while True:
+
         for event in pg.event.get():
+
             if event.type == pg.QUIT: return
+
         
         key_lst = pg.key.get_pressed()
-        if key_lst[pg.K_UP]:
-            kk_rct.move_ip(0, -1)
-        if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip(0, 1)
-        if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip(-1, 0)
-        if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(1, 0)
+        dx = -1  # 基本の移動量（左に流れる）
+        dy = 0
+        
+        if key_lst[pg.K_UP]:    
+            dy -= 1
+        if key_lst[pg.K_DOWN]:  
+            dy += 1
+        if key_lst[pg.K_LEFT]:  
+            dx -= 1
+        if key_lst[pg.K_RIGHT]: 
+            dx += 2 
+
 
         x = tmr % 3200 #練習5
+
         screen.blit(bg_img, [-x, 0]) #練習2
+
         screen.blit(bg_img2, [-x+1600, 0])#練習4   
+
         screen.blit(bg_img, [-x+3200, 0])#練習7
+
         screen.blit(kk_img, kk_rct)
+
         pg.display.update()
+
         tmr += 1        
+
         clock.tick(200) #練習6
 
 
+
+
+
 if __name__ == "__main__":
+
     pg.init()
     main()
     pg.quit()
